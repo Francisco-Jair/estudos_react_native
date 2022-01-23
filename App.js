3632c3bbb077dd96f53059325df507e990b2c4e4
Francisco Jair <fjair123@ufpi.edu.br>
2022-01-22 23:29:14 -0300

    Definido varios componentes no mesmo arquivo

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { Text, SafeAreaView } from "react-native";
 import Primeiro from "./src/componentes/Primeiro";
+import Comp, { Comp1, Comp2 } from "./src/componentes/Multi";
 
 export default function App() {
   return (
     <SafeAreaView>
-      <Primeiro />
+      <Comp />
+      <Comp1 />
+      <Comp2 />
+      {/* <Primeiro /> */}
     </SafeAreaView>
   );
 }
---
+++ src/componentes/Multi.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function Comp() {
+  return <Text>Comp #Oficial</Text>;
+}
+
+function Comp1() {
+  return <Text>Comp #1</Text>;
+}
+
+function Comp2() {
+  return <Text>Comp #2</Text>;
+}
+
+function Comp3() {
+  return <Text>Comp #3</Text>;
+}
+
+export { Comp1, Comp2 };
