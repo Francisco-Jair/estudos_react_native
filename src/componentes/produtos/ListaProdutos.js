58f5e3c8f3792e6d4d3521395b8db6a227bf5450
Francisco Jair <fjair123@ufpi.edu.br>
2022-01-23 13:47:23 -0300

    FlatList

--- src/componentes/produtos/ListaProdutos.js
+++ src/componentes/produtos/ListaProdutos.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, FlatList } from "react-native";
 import estilo from "../../estilos/estilo";
 
 import produtos from "./produtos";
@@ -8,13 +8,17 @@ export default function ListaDeProdutos(props) {
   return (
     <>
       <Text style={estilo.grande}>Lista de Produtos</Text>
-      {produtos.map((p) => {
-        return (
-          <Text key={p.id}>
-            {p.id} {p.nome}
-          </Text>
-        );
-      })}
+      <FlatList
+        keyExtractor={(i) => String(i.id)}
+        data={produtos}
+        renderItem={({ item: p }) => {
+          return (
+            <Text>
+              {p.id} {p.nome}
+            </Text>
+          );
+        }}
+      />
     </>
   );
 }
