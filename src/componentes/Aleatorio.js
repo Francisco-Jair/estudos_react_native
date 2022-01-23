31412c43cf521699d89664aa57d0ba5a8414c974
Francisco Jair <fjair123@ufpi.edu.br>
2022-01-23 00:01:43 -0300

    Caracteristica de props

--- src/componentes/Aleatorio.js
+++ src/componentes/Aleatorio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text } from "react-native";
 
+// O props é um objeto de leitura, não da para modificar seus valores
 export default function Aleatorio({ min, max }) {
   const valorAleatorio = parseInt(Math.random() * (max - min)) + min;
 
